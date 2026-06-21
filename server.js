4bae801297a78b5c9e23be9e76890a50f733b1c9
Husanpreet Singh <preethusan1807@gmail.com>
2026-06-21 17:54:51 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -420,4 +420,4 @@ app.get('/api/server-info', (req, res) => {
     res.json({ port: PORT, ips });
 });
 
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server is running on port ${PORT}`));
